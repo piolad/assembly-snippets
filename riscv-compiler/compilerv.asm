9636379b42a011945d842f8e0401da434f510e29
Piotr Ladkowski <piolad2003@gmail.com>
2024-12-14 14:23:14 +0100

    add recognition of the number of the first register

--- riscv-compiler/compilerv.asm
+++ riscv-compiler/compilerv.asm
@@ -40,24 +40,23 @@ openfile:
 	call	refill_buffer
 
 start_read_inst:
-	mv	s0, zero # instruction input data
-	mv	s1, zero # argument 1 input data
-	mv	s6, zero # argument 2 input data
-	mv	s7, zero # argument 3 input data
+	mv	s0, zero # instruction input data / 1st input data
+	mv	s1, zero # argument 2 input data
+	mv	s6, zero # argument 3 input data
 read_inst:
-	lb	t1, (a1)
-	bnez	t1, bufok
+	lb	s7, (a1)
+	bnez	s7, bufok
 	
 	# refill buffer if empty
 	call    refill_buffer
 bufok:
 	addi	a1, a1, 1
 	# check if whitespace character
-	beq	t1, s2, whitespaceChar	# ' '
-	beq	t1, s3, whitespaceChar	# '\t'
+	beq	s7, s2, whitespaceChar	# ' '
+	beq	s7, s3, whitespaceChar	# '\t'
 	
 	# newline
-	beq	t1, s4, newlineChar	# TODO: here we assume only LF, there maybe arror with CRLF ending
+	beq	s7, s4, newlineChar	# TODO: here we assume only LF, there maybe arror with CRLF ending
 	
 	# if instruction > bin(100000 00000000 00000000), 4 instructions already packed
 	# so either instruction is wrong or sltiu edge case
@@ -66,7 +65,7 @@ bufok:
 	# also add converting to lowercase and removing weird characters (e.g. CR, etc)
 	# pack up to 4 bytes into 1 32bit register
 	slli	s0, s0, 7
-	add	s0, s0, t1
+	add	s0, s0, s7
 	
 	j	read_inst
 
@@ -160,40 +159,110 @@ no_imm_end:
 	add	a6, a6, t5
 	
 
+
+	mv	s0, zero	# reset s0 for first argument
 ###########################
 # todo: rewrite (and finish) this as a function;
 # fun will be called 2-3 times and accept separators of ',' and '\n' while saving last letter 
 # after function call, comparison whether newline occured after e.g. only 2 args
 ###########################
 f_x_before_1st_arg:
-	lb	t1, (a1)
-	bnez	t1, bufok1
+	lb	s7, (a1)
+	bnez	s7, bufok1
 	
 	call	refill_buffer
 	
 bufok1:
-	# check for 'x' in current position. if space/tab - skip. if newline -  go to no_args
-	beq	t1, s9, rd_1st_arg
+	addi	a1, a1, 1
+	# if space/tab - skip. if newline -  go to no_args. if anything else - syntax error
 	
-	beq	t1, s2, f_x_before_1st_arg	# ' '
-	beq	t1, s3, f_x_before_1st_arg	# '\t'
+	beq	s7, s2, f_x_before_1st_arg	# ' '
+	beq	s7, s3, f_x_before_1st_arg	# '\t'
 	
 	# newline - end of instruction - wrong instruction
-	beq	t1, s4, no_args 	# TODO: here we assume only LF, there maybe arror with CRLF ending
+	beq	s7, s4, no_args 	# TODO: here we assume only LF, there maybe arror with CRLF ending
+	
+	bne	s7, s9, syntax_e	# not whitespace, can only be 'x' or syntax error
+		
+	call	rd_int12b
+	
+	# TODO: after calling perform additonal checks like if the last chracter is ' ' or ',' - then ok. Otherwiser - syntax error
 
 
-rd_1st_arg:
+# function: rd_int12b
+# get first num, upto 12 bits long
+rd_int12b:
+	mv	a3, zero	# converted number
+	mv	a4, zero	#  sign_info; at the same indicates largest possible number in terms of abosolute value (2048 for negative, 2047 for positive)
 	
+	# fix constatnt temporary registers after call
+	li	t3, 10
+	li	t4, '-'
+	li	t5, '0'
+	li	t6, '9'
 	
+loop_rdint:
+	lb	s7, (a1)
+	bnez	s7, bufok_rdint
+	
+
+	mv	s10, ra		# non-leaf procedure - save the return address
+	call	refill_buffer
+	mv	ra, s10
+	
+	# fix constatnt temporary registers after call
+	li	t3, 10
+	li	t4, '-'
+	li	t5, '0'
+	li	t6, '9'
+
+	
+bufok_rdint:
+	addi	a1, a1, 1
+	beqz	a4, check_minus	# minus unset - allow to set it
+	
+	# check for minus in the middle of numer e.g. 102-23, --12, 1234-
+	beq	s7, t4, syntax_e
+	
+convert_num:
+	bgt	s7, t6, end_rdint
+	blt	s7, t5, end_rdint
+	
+	mul	a3, a3, t3 	# multiply by 10
+	add	a3, a3, s7
+	sub	a3, a3, t5
+	
+	# range check:
+	bgtu	a3, a4, syntax_e
+	
+	j	loop_rdint
+	
+check_minus:
+	li	a4, 2047
+	bne	s7, t4, convert_num	# positive number -> continue with normal loop
+
+	li	a4, 2048	# number is negative
+	j 	loop_rdint	# skip minus - go to loop beginnig
+	
+end_rdint:
+	mv	a0, a3
+	addi	a4, a4, -2047
+	
+	beqz	a4, ret_rdint
+	sub	a0, zero, a0
+ret_rdint:
+	ret
+
+#==================================================================	
 	
 	
 chk_sltiu:
 	# (edge case of instruction interpretation)
-	# check if t1 is equal to 'u'
+	# check if s7 is equal to 'u'
 	# and if current content of s0 is stli
 	# otherwise - write error and go to next line
 #	bne	s0, __, label
-#	bne	t1, __, label
+#	bne	s7, __, label
 #	addi	s0, s0, 
 #	j	write-error and sth
 
@@ -216,14 +285,18 @@ refill_buffer:		# assumption: s11 contains file descriptor
         ecall
 
         # If no data read, return -1
-        blez    a0, exit
+        blez    a0, exit	# this will validatae ra but we do not care - this is only for exiting
 
         # Add '\0' after the last byte
         la      t0, buf        # t0 = address of buf
         add     t0, t0, a0     # t0 = buf + (number_of_bytes_read)
         sb      zero, 0(t0)    # *(buf + a0) = '\0'
-       	lb	t1, (a1)
+       	lb	s7, (a1)
         ret
+
+
+skip_to_nline:
+# todo: implement this
 
 #===========================================================
 # wrong instruciton - print info and go to newline or end
@@ -231,6 +304,7 @@ bad_instr:
 	la 	a0, b_ins
 	li	a7, SYS_PRT
 	ecall
+	call	skip_to_nline
 	j	start_read_inst
 f_nline:
 	# todo: what if crlf is used?
@@ -240,4 +314,12 @@ no_args:
 	la 	a0, n_args
 	li	a7, SYS_PRT
 	ecall
+	call	skip_to_nline
 	j	start_read_inst
+
+syntax_e:
+	la 	a0, b_sntx
+	li	a7, SYS_PRT
+	ecall
+	call	skip_to_nline
+	j	start_read_inst	
