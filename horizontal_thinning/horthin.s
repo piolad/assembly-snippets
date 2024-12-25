0aa80c2e3058b1ef0ce3f5aaa19c8944b1f116cd
piolad <piolad2003@gmail.com>
2024-12-25 15:40:37 +0100

    fix prologue, add basic file reading

--- horizontal_thinning/horthin.s
+++ horizontal_thinning/horthin.s
@@ -1,22 +1,18 @@
         section .text
         global  horthin
 horthin:
-; prologue – same for all routines
-        push ebp ; save caller's frame pointer
-        mov ebp, esp ; set own frame pointer
-        ; at this point ESP == n*16 + 8
-        sub esp, (LOCAL_DATA_SIZE + 3) & ~3 ; allocate locals if needed
-        ; save “saved” registers if used
+        push ebp
+        mov ebp, esp
+        ; sub esp, (LOCAL_DATA_SIZE + 3) & ~3 ; allocate locals if needed
         push ebx
         push esi
         push edi
-        ; function body - procedure-specific
-        ; ...
-        ; epilogue – same for all routines
-        ; restore saved registers if they were saved in the prologue
+
+        
+fin:
         pop edi
         pop esi 
         pop ebx
-        mov esp, ebp ; discard local vars if they were allocated
-        pop ebp ; restore caller's frame pointer
-        ret ; return to calle
+        mov esp, ebp ; local vars 
+        pop ebp 
+        ret
--- horizontal_thinning/main.c
+++ horizontal_thinning/main.c
@@ -5,5 +5,16 @@ void horthin(void *img, uint32_t width, uint32_t height);
 
 int main(int argc, char *argv[])
 {
+    FILE *fptr;
+    for(int i=0; i<argc; i++){
+        if(!(fptr = fopen("./newprogram.txt","rb"))){
+            printf("file \"%s\" could not be opened");
+            continue;
+        }
+
+        //....
+
+        fclose(fptr); 
+    }
     
 }
